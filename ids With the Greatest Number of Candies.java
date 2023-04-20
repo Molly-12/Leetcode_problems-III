325a24902d77fc58bfd0d314552dd95fde98347e
Molly-12 <92814851+Molly-12@users.noreply.github.com>
2023-04-20 21:41:10 +0530

    Create ids With the Greatest Number of Candies.java

---
+++ ids With the Greatest Number of Candies.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+      int max=0;
+      for(int i:candies){
+        if(i>max){
+          max=i;
+        }
+      }
+      List<Boolean> list= new ArrayList();
+      for(int i:candies){
+        list.add(i+extraCandies>=max);
+      }
+      return list;
+    }
+}
+
+    
